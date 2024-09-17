b6d130688ceb87719ad5edf703bbcbd59073510a
chichi-gaga <shitg5066@gmail.com>
2024-09-18 00:56:10 +0800

    Add files via upload

---
+++ practice1.9.kt
@@ -0,0 +1,9 @@
+//Практическая работа 1.9
+//Емельянов Иван ИС223
+fun main() {
+    val number1 = 13
+    val number2 = 44
+    val number3 = 68
+    val number4 = 92
+    println("$number1 $number2 $number3 $number4")
+}
